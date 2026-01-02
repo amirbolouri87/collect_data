d7855f1c2fd05333e9aff21c975a03bf58d21f0d
matoogsm <Matoogsm90@gmail.com>
2026-01-02 22:43:40 +0330

    ADD:celery

--- config/celery.py
+++ config/celery.py
@@ -1,72 +1,64 @@
-from celery import Celery
+# import os
+# import asyncio
+#
+# from celery import Celery
+# from django.conf import settings
+# from .tasks import *
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+#
+# app = Celery(
+#     'config',
+#     broker=f'amqp://{settings.RABBITMQ_USERNAME}:{settings.RABBITMQ_PASSWORD}@{settings.RABBITMQ_HOST}:{settings.RABBITMQ_PORT}//',
+#     backend='rpc://'
+# )
+# app.autodiscover_tasks()
+#
+# app.conf.update(
+#     task_serializer='json',
+#     result_serializer='json',
+#     accept_content=['json'],
+#     # worker_prefetch_multiplier=4,
+#     timezone='Asia/Tehran',
+#     enable_utc=True,
+# )
+#
+#
+#
+# @app.task
+# def scrape_tgju_table():
+#     asyncio.run(combined_crawl())
+#
+#
+# @app.on_after_configure.connect
+# def setup_periodic_tasks(sender, **kwargs):
+#     # Calls test_print('hello') every 10 seconds
+#     # sender.add_periodic_task(10.0, test_print.s('hello'), name='add every 10')
+#     sender.add_periodic_task(60.0, scrape_tgju_table.s(), name='scrape_tgju_table_60s')
 import os
-import requests
 import asyncio
-import json
-from playwright.async_api import async_playwright
+from celery import Celery
 from django.conf import settings
-
-async def insert_data(data):
-    import requests
-    from elasticsearch import Elasticsearch
-
-    es = Elasticsearch("http://192.168.88.229:9200")
-
-    # doc = json.loads(data)
-    await asyncio.to_thread(
-        es.index,
-        index="text_data",
-        document={"content": data}
-    )
-    print('inserted . ')
-
-async def handle_response(response):
-    url = response.url
-    if "ajax.json" in url:
-        try:
-            r = requests.get(url)
-            #print(r.text)
-            await insert_data(r.text)
-
-        except Exception as e:
-            print("Error fetching URL:", e)
-
-
-async def combined_crawl():
-    async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=False)
-        page = await browser.new_page()
-
-        # wrap async handler for Playwright events
-        page.on("response", lambda resp: asyncio.create_task(handle_response(resp)))
-
-        await page.goto(settings.COMBINED_CRAWL_URL)
-
-        # async wait just like your time.sleep(2)
-        await asyncio.sleep(2)
-
-        await browser.close()
+from .crawl import combined_crawl
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-app = Celery('config')
-
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
-
 app = Celery(
-    'scrape_tgju_table',
-    broker=F'amqp://{settings.RABBITMQ_USERNAME}:{settings.RABBITMQ_PASSWORD}@{settings.RABBITMQ_HOST}:{settings.RABBITMQ_PORT}//',
+    'config',
+    broker=f'amqp://{settings.RABBITMQ_USERNAME}:{settings.RABBITMQ_PASSWORD}@{settings.RABBITMQ_HOST}:{settings.RABBITMQ_PORT}//',
     backend='rpc://'
 )
+app.conf.update(
+    task_serializer='json',
+    result_serializer='json',
+    accept_content=['json'],
+    timezone='Asia/Tehran',
+    enable_utc=True,
+)
+app.config_from_object("django.conf:settings", namespace="CELERY")
+app.autodiscover_tasks()
 
-@app.on_after_configure.connect
-def setup_periodic_tasks(sender, **kwargs):
-    sender.add_periodic_task(30.0, scrape_tgju_table.s(), name='scrape_tgju_table')
 
 @app.task
 def scrape_tgju_table():
     asyncio.run(combined_crawl())
-
-
-
--- config/settings/environments/development.py
+++ config/settings/environments/development.py
@@ -4,3 +4,10 @@ from ..packages import *
 
 DEBUG = BaseConfig.DEBUG
 ALLOWED_HOSTS = BaseConfig.ALLOWED_HOSTS
+
+CELERY_BEAT_SCHEDULE = {
+    "scrape-tgju-every-90s": {
+        "task": "config.celery.scrape_tgju_table",
+        "schedule": 30.0,
+    },
+}
